d7c92bd6d969ae4e6fe7e034209cc02602e32806
pietdaniel <pietdaniel@gmail.com>
2014-10-04 17:01:45 -0400

    user timeline

--- mach2/app.py
+++ mach2/app.py
@@ -1,9 +1,7 @@
-from flask import Flask
+from flask import Flask, session, redirect, request, flash
 from flask import session
 from flask_oauth import OAuth
-from flask import redirect
-from flask import request
-from flask import flash
+import pickle
 
 app = Flask(__name__)
 app.debug = True
@@ -47,6 +45,8 @@ def get_timeline(token=None):
     resp = twitter.get("statuses/user_timeline.json")
     if resp.status == 200:
       tweets = resp.data
+      user = session['twitter_user']
+      pickle.dump( tweets, open( user + "_timeline.p", "wb" ) )
     else:
       return str(resp.status)
     return str(resp.data)
