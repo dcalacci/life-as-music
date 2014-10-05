95443d586a96da6c71aadcb0aa73c5566f3cfabb
pietdaniel <pietdaniel@gmail.com>
2014-10-05 08:51:07 -0400

    updates

---
+++ mithacks-project/clojure_hacks/spike/project.clj
@@ -0,0 +1,7 @@
+(defproject spike "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License"
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.csv "0.1.2"]
+                 [overtone "0.9.1"]]
+  :main spike.core)
