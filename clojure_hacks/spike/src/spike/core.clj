f46539b81e2f79d0d4cff37d3ad11dae4d9313ba
pietdaniel <pietdaniel@gmail.com>
2014-10-05 08:46:54 -0400

    cleaning

--- clojure_hacks/spike/src/spike/core.clj
+++
@@ -1,9 +0,0 @@
-(ns spike.core)
-(use 'overtone.core)
-(boot-external-server)
-
-(require '[overtone.core])
-
-(definst foo [] (saw 220))
-
-(foo)
--- clojure_hacks/spike/test/spike/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns spike.test.core
-  (:use [spike.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
